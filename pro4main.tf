78da1639b85484f4753cdb98b343be9f56981db8
Musa0813 <kmusas2024@gmail.com>
2025-04-19 23:40:47 -0500

    instance, 02

--- pro4main.tf
+++ pro4main.tf
@@ -1,13 +1,11 @@
 resource "aws_instance" "kamara" {
-    count = "2"
-    ami = "ami-3457-54321"
-    instance_type = ["t2.nano", "t2.micro", "t2.medium", "t2.large"]
-    availability_zone = ["us-east-1a", "us-east-1b", "us-east-1c"]
-    enable_primary_ipv6 = false
+    count = var.number_instances
+    ami = var.ami_id
+    instance_type = var.instance_type[1]
+    availability_zone = var.availability_zone[1]
+    enable_primary_ipv6 = var.enable_primary_ipv6
     security_groups = "default"
-    tags = {
-        "Name" = "kamara"
-    }
+    tags = var.tags
 
   
 }
